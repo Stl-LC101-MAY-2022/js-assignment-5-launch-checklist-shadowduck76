07caec373f90e83d9fbe47042dbc49da9d5e9903
Jamie Shadduck <jamie.shadduck.js@gmail.com>
2022-08-07 15:40:15 -0500

    submit for review

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
 
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoLevel = document.querySelector("input[name=cargoLevel]");
+   
+    formSubmission(this.document, form, pilot, copilot, fuelLevel, cargoLevel);
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+        let planet = pickPlanet(listedPlanets);
+        addDestinationInfo(this.document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+    })
    
 });
